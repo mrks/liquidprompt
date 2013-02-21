00b30206241aa0e46c25304a8c249124e6a18eb2
Markus Dreseler <markus@dreseler.de>
2013-02-20 16:00:23 -0800

    Added information when running in screen

--- liquid.ps1
+++ liquid.ps1
@@ -24,7 +24,7 @@
 # add time, jobs, load and battery
 LP_PS1="${LP_PS1_PREFIX}${LP_TIME}${LP_BATT}${LP_LOAD}${LP_JOBS}"
 # add user, host and permissions colon
-LP_PS1="${LP_PS1}[${LP_USER}${LP_HOST}${LP_PERM}"
+LP_PS1="${LP_PS1}[${LP_USER}${LP_HOST}${LP_PERM}${LP_SCREEN}bla"
 
 # if not root
 if [[ "$EUID" -ne "0" ]]
